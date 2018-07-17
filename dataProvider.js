fdb28801376306bb32c5bb0299b7964c595df26b
Nigel Scott <nscott@asl-control.co.uk>
2018-07-17 14:38:54 +0100

    First stab
    - can receive and send
    
    TODO:
    - colour names
    - avatars
    - tree icons
    - persist counts
    - panel icon
    - notifications
    - configurable format
    - button for post
    - check output panel is visible (visible editors)

---
+++ dataProvider.js
@@ -0,0 +1,153 @@
+/* jshint esversion:6 */
+
+Object.defineProperty( exports, "__esModule", { value: true } );
+
+var vscode = require( 'vscode' );
+
+var servers = [];
+
+const SERVER = "server";
+const CHANNEL = "channel";
+
+function findServer( e )
+{
+    return e.type === SERVER && e.id.toString() === this.toString();
+};
+
+function findChannel( e )
+{
+    return e.type === CHANNEL && e.id.toString() === this.toString();
+};
+
+class DiscordChatDataProvider
+{
+    constructor( _context )
+    {
+        this._context = _context;
+
+        this._onDidChangeTreeData = new vscode.EventEmitter();
+        this.onDidChangeTreeData = this._onDidChangeTreeData.event;
+    }
+
+    getChildren( element )
+    {
+        if( !element )
+        {
+            if( servers.length > 0 )
+            {
+                return servers;
+            }
+            return [ { name: "Houston - we have a problem..." } ];
+        }
+        else if( element.type === SERVER )
+        {
+            return element.channels;
+        }
+        else if( element.type === CHANNEL )
+        {
+            return element.name;
+        }
+    }
+
+    getTreeItem( element )
+    {
+        var treeItem = new vscode.TreeItem( element.name );
+
+        treeItem.id = element.id;
+
+        treeItem.collapsibleState = vscode.TreeItemCollapsibleState.None;
+
+        if( element.type === SERVER )
+        {
+            treeItem.collapsibleState = vscode.TreeItemCollapsibleState.Collapsed;
+        }
+        else if( element.type === CHANNEL )
+        {
+            treeItem.command = {
+                command: "discord-chat.openChannel",
+                title: "",
+                arguments: [
+                    element.channel
+                ]
+            };
+
+        }
+
+        if( element.unreadCount && element.unreadCount > 0 )
+        {
+            treeItem.label = element.name + " (" + element.unreadCount + ")";
+        }
+
+        return treeItem;
+    }
+
+    clear()
+    {
+        servers = [];
+    }
+
+    populate( channels )
+    {
+        channels.map( function( channel )
+        {
+            if( channel.guild && channel.type === "text" )
+            {
+                var server = servers.find( findServer, channel.guild.id );
+                if( server === undefined )
+                {
+                    server = { type: SERVER, name: channel.guild.name, channels: [], id: channel.guild.id, unreadCount: 0 };
+                    servers.push( server );
+                }
+
+                var channelItem = server.channels.find( findChannel, channel.id );
+                if( channelItem === undefined )
+                {
+                    channelItem = { type: CHANNEL, name: channel.name, channel: channel, users: [], id: channel.id, unreadCount: 0 };
+                    server.channels.push( channelItem );
+                }
+            }
+        } );
+    }
+
+    updateServerCount( server )
+    {
+        var total = 0;
+        server.channels.map( channel =>
+        {
+            total += channel.unreadCount;
+        } );
+        server.unreadCount = total;
+        this._onDidChangeTreeData.fire();
+    }
+
+    updateChannelCount( channel, increment )
+    {
+        var server = servers.find( findServer, channel.guild.id );
+        if( server )
+        {
+            var channelItem = server.channels.find( findChannel, channel.id );
+            if( channelItem )
+            {
+                channelItem.unreadCount = increment ? channelItem.unreadCount + 1 : 0;
+                this.updateServerCount( server );
+            }
+        }
+    }
+
+    update( message )
+    {
+        this.updateChannelCount( message.channel, +1 );
+    }
+
+    clearUnread( channel )
+    {
+        this.updateChannelCount( channel, 0 );
+    }
+
+    refresh()
+    {
+        this._onDidChangeTreeData.fire();
+    }
+}
+
+exports.DiscordChatDataProvider = DiscordChatDataProvider;
---
+++ extension.js
@@ -0,0 +1,122 @@
+/* jshint esversion:6 */
+
+const discord = require( 'discord.js' );
+const vscode = require( 'vscode' );
+var treeView = require( "./dataProvider" );
+
+var outputChannels = {};
+var currentChannel;
+
+function activate( context )
+{
+    const client = new discord.Client();
+
+    var provider = new treeView.DiscordChatDataProvider( context );
+    // var status = vscode.window.createStatusBarItem( vscode.StatusBarAlignment.Left, 0 );
+    var generalOutputChannel = vscode.window.createOutputChannel( 'discord-chat' );
+
+    function register()
+    {
+        vscode.window.registerTreeDataProvider( 'discord-chat', provider );
+        vscode.window.registerTreeDataProvider( 'discord-chat-explorer', provider );
+
+        context.subscriptions.push(
+            vscode.commands.registerCommand( 'discord-chat.openChannel', ( channel ) =>
+            {
+                var outputChannelName = 'discord-chat.' + channel.guild.name + '.' + channel.name;
+                currentChannel = channel;
+
+                var outputChannel = vscode.window.createOutputChannel( outputChannelName );
+                outputChannels[ outputChannelName ] = outputChannel;
+                context.subscriptions.push( outputChannel );
+
+                var entries = [];
+                channel.fetchMessages( { limit: vscode.workspace.getConfiguration( 'discord-chat' ).history } ).then( function( messages )
+                {
+                    messages.map( function( message )
+                    {
+                        entries.push( message.author.username + ": " + message.content );
+                    } );
+
+                    entries.reverse().map( function( entry )
+                    {
+                        outputChannel.appendLine( entry );
+                    } );
+
+                    outputChannel.show( false );
+
+                    provider.clearUnread( channel );
+                } );
+
+            } ) );
+
+        context.subscriptions.push(
+            vscode.commands.registerCommand( 'discord-chat.post', function()
+            {
+                vscode.window.showInputBox( { prompt: "Post message to " + currentChannel.name } ).then(
+                    function( message )
+                    {
+                        currentChannel.send( message );
+                    } );
+            } ) );
+
+        context.subscriptions.push( vscode.workspace.onDidChangeConfiguration( function( e )
+        {
+            if( e.affectsConfiguration( "discord-chat" ) )
+            {
+                vscode.commands.executeCommand( 'setContext', 'discord-chat-in-explorer', vscode.workspace.getConfiguration( 'discord-chat' ).showInExplorer );
+            }
+        } ) );
+
+        context.subscriptions.push( generalOutputChannel );
+
+        vscode.commands.executeCommand( 'setContext', 'discord-chat-in-explorer', vscode.workspace.getConfiguration( 'discord-chat' ).showInExplorer );
+
+        client.on( 'ready', () =>
+        {
+            generalOutputChannel.appendLine( `Logged in as ${client.user.tag}!` );
+            provider.populate( client.channels );
+            provider.refresh();
+        } );
+
+        client.on( 'message', message =>
+        {
+            if( message.channel.type === "text" )
+            {
+                if( message.channel === currentChannel )
+                {
+                    var outputChannelName = 'discord-chat.' + message.channel.guild.name + '.' + message.channel.name;
+                    var outputChannel = outputChannels[ outputChannelName ];
+                    if( outputChannel )
+                    {
+                        outputChannel.appendLine( message.author.username + ": " + message.content );
+                    }
+                    provider.clearUnread( message.channel );
+                }
+                else
+                {
+                    provider.update( message );
+                }
+            }
+        } );
+
+        var token = vscode.workspace.getConfiguration( 'discord-chat' ).token;
+        if( token )
+        {
+            client.login( token );
+        }
+        else
+        {
+            vscode.window.showInformationMessage( "Please set discord-chat.token" );
+        }
+    }
+
+    register();
+}
+
+function deactivate()
+{
+}
+
+exports.activate = activate;
+exports.deactivate = deactivate;
