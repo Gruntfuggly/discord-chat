a6beaecca7a34d28c6a474fd6f85f002b5c8797a
Nigel Scott <nscott@asl-control.co.uk>
2018-08-28 16:43:35 +0100

    Make debug logging configurable
    - turn it off by default as it exposes the user's token and therefore gets
    logging in vscode's logs

--- extension.js
+++ extension.js
@@ -472,7 +472,7 @@ function activate( context )
         {
             function showNotification()
             {
-                var notify = vscode.workspace.getConfiguration( 'discord-chat' ).notify;
+                var notify = vscode.workspace.getConfiguration( 'discord-chat' ).get( 'notify' );
                 if( notify === "always" ||
                     ( notify == "whenHidden" &&
                         ( discordChatExplorerView.visible === false && discordChatView.visible === false ) ) )
@@ -859,7 +859,10 @@ function activate( context )
         } );
         client.on( 'debug', message =>
         {
-            generalOutputChannel.appendLine( "debug: " + message );
+            if( vscode.workspace.getConfiguration( 'discord-chat' ).get( 'debug' ) === true )
+            {
+                generalOutputChannel.appendLine( "debug: " + message );
+            }
         } );
 
         client.on( 'ready', () =>
