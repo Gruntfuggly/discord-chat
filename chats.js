f6dfaa13bd01a138895ce8a8870f51168954d6b4
Nigel Scott <nscott@asl-control.co.uk>
2018-10-03 13:04:44 +0100

    more wip

--- chats.js
+++ chats.js
@@ -1,4 +1,112 @@
+var strftime = require( 'strftime' );
+var utils = require( './utils' );
+
 var messages = {};
+
+function sanitizeUnicode( entries )
+{
+    entries = entries.map( function( entry )
+    {
+        entry = entry.replace( '’', '\'' );
+        entry = entry.replace( '‘', '\'' );
+        entry = entry.replace( '“', '"' );
+        entry = entry.replace( '”', '"' );
+        entry = entry.replace( '–', '-' );
+        entry = entry.replace( '—', '-' );
+        return entry;
+    } );
+    return entries;
+}
+
+function formatMessage( message, compact, short )
+{
+    function separator()
+    {
+        return compact === true ? ": " : ":\n ";
+    }
+
+    function content( text )
+    {
+        return text.split( '\n' ).join( compact ? "\n" : "\n " );
+    }
+
+    function isCode( text )
+    {
+        var codeRegex = new RegExp( "```(.+)\n((.*\n)*)```" );
+        var result = codeRegex.exec( text );
+        return result;
+    }
+
+    var entries = [];
+
+    var format = "%a %T";
+    if( short )
+    {
+        format = "%H:%M";
+    }
+    else if( new Date() - message.createdAt > 1000 * 60 * 60 * 24 * 7 )
+    {
+        format = "%D %T";
+    }
+
+    var header =
+        strftime( format, message.createdAt ) +
+        ( short ? ( " [" + utils.toServerName( message.channel ) + "/" + utils.toChannelName( message.channel ) + "]" ) : "" ) +
+        " @" + message.author.username;
+
+    if( message.author )
+    {
+        var code = isCode( message.content );
+
+        if( message.type == "GUILD_MEMBER_JOIN" )
+        {
+            entries.push( header + " joined" );
+        }
+        else if( code )
+        {
+            entries.push( header + " posted some " + code[ 1 ] + " code:\n" + code[ 2 ] );
+        }
+        else if( message.cleanContent )
+        {
+            entries.push( header + separator() + content( message.cleanContent ) );
+        }
+        else if( message.content )
+        {
+            entries.push( header + separator() + content( message.content ) );
+        }
+        else if( message.embeds.length > 0 )
+        {
+            message.embeds.map( function( embed )
+            {
+                if( embed.image )
+                {
+                    entries.push( header + " embedded image " + embed.image.url + " (" + embed.description + ")" );
+                }
+                if( embed.video )
+                {
+                    entries.push( header + " embedded video " + embed.video.url + " (" + embed.description + ")" );
+                }
+            } );
+        }
+
+        if( message.attachments )
+        {
+            message.attachments.map( function( attachment )
+            {
+                entries.push( header + " attached " + attachment.url );
+            } );
+        }
+
+        if( compact !== true && short !== true )
+        {
+            entries.push( "" );
+        }
+    }
+
+    entries = sanitizeUnicode( entries );
+
+    return entries;
+}
 
 function addMessage( id, message, timeOfMessage )
 {
@@ -48,6 +156,7 @@ function getUnreadMessages( id )
     return [];
 }
 
+module.exports.formatMessage = formatMessage;
 module.exports.addMessage = addMessage;
 module.exports.chatRead = chatRead;
 module.exports.getReadMessages = getReadMessages;
